a64cc6d1eb9143a185c62e5a3561f54dea1cbccc
Commonly <51011212+commonly-ts@users.noreply.github.com>
2025-11-30 07:19:12 +1300

    fix: attempt to fix

--- src/core/registry/event.registry.ts
+++ src/core/registry/event.registry.ts
@@ -1,5 +1,5 @@
 import { ClientEvents, Collection } from "discord.js";
-import { getCorePath } from "src/shared/utility/functions.js";
+import { exists, getCorePath } from "src/shared/utility/functions.js";
 import { BaseRegistry, Event } from "src/shared/typings/index.js";
 import { promises as fs } from "fs";
 import { join } from "node:path";
@@ -8,17 +8,18 @@ import TriviousClient from "../client/trivious.client.js";
 export default class EventRegistry extends BaseRegistry<Event> {
 	protected items = new Collection<string, Event>();
 	async load(directory: string = getCorePath({ coreDirectory: "events" })): Promise<this> {
-		const exists = await fs.stat(directory).then(() => true).catch(() => false);
-		if (!exists) return this;
+		if (!(await exists(directory))) return this;
 
 		const entries = await fs.readdir(directory, { withFileTypes: true });
-
 		for (const entry of entries) {
 			const fullPath = join(directory, entry.name);
 
 			if (entry.isDirectory()) {
 				await this.load(fullPath);
-			} else if (entry.isFile() && entry.name.endsWith(".js")) {
+				continue;
+			}
+
+			if (entry.isFile() && entry.name.endsWith(".js")) {
 				const event = await this.importFile(fullPath);
 				if (!event) continue;
 
