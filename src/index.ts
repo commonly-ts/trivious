74e305bb5574e9fea5ff9123e5dc4ec2ac30e71a
Commonly <51011212+commonly-ts@users.noreply.github.com>
2025-11-30 03:36:45 +1300

    Add main index file for exports
    Introduces src/index.ts to centralize and re-export core types, classes, and utilities for easier package consumption. Includes exports for client, commands, components, typings, cached interactions, and common discord.js objects.

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import type {
+	CacheType,
+	ChatInputCommandInteraction as DJS_ChatInputCommandInteraction,
+	ButtonInteraction as DJS_ButtonInteraction,
+	StringSelectMenuInteraction as DJS_StringSelectMenuInteraction,
+	ModalSubmitInteraction as DJS_ModalSubmitInteraction,
+} from "discord.js";
+
+// Client
+export { default as TriviousClient } from "./core/client/trivious.client.js";
+
+// Commands
+export { default as Command } from "./core/commands/command.base.js";
+export { default as Subcommand } from "./core/commands/subcommand.base.js";
+export { default as CommandRegistry } from "./core/registry/command.registry.js";
+export type { CommandBuilder } from "./core/commands/command.base.js";
+export type { SubcommandBuilder } from "./core/commands/subcommand.base.js";
+
+// Components
+export { default as Component } from "./core/components/component.base.js";
+export { default as ComponentRegistry } from "./core/registry/component.registry.js";
+export type { ComponentBuilder } from "./core/components/component.base.js";
+
+// Typings
+export * from "./shared/typings/index.js";
+
+// Cached Interactions
+export type ChatInputCommandInteraction = DJS_ChatInputCommandInteraction<CacheType>;
+export type ButtonInteraction = DJS_ButtonInteraction<CacheType>;
+export type StringSelectMenuInteraction = DJS_StringSelectMenuInteraction<CacheType>;
+export type ModalSubmitInteraction = DJS_ModalSubmitInteraction<CacheType>;
+
+// Common discord.js things
+export { Collection, ClientEvents } from "discord.js";
