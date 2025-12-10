a8e09b4be267c086d987a4be4c3eb3934c31a415
Commonly <51011212+commonly-ts@users.noreply.github.com>
2025-12-10 14:59:58 +1300

    chore(docs): utility builder jsdoc comments

--- src/core/builders/util.builders.ts
+++ src/core/builders/util.builders.ts
@@ -1,9 +1,24 @@
 import { ActionRowBuilder, APIEmbed, EmbedBuilder, EmbedData, MessageActionRowComponentBuilder } from "discord.js";
 
+/**
+ * Utility action row builder
+ *
+ * @export
+ * @template {MessageActionRowComponentBuilder} T
+ * @param {...T[]} builders
+ * @returns {*}
+ */
 export function createActionRow<T extends MessageActionRowComponentBuilder>(...builders: T[]) {
 	return new ActionRowBuilder<T>().setComponents(...builders);
 }
 
+/**
+ * Utility embed builder
+ *
+ * @export
+ * @param {?(EmbedData | APIEmbed)} [data]
+ * @returns {*}
+ */
 export function createEmbed(data?: EmbedData | APIEmbed) {
 	return new EmbedBuilder(data);
 }
