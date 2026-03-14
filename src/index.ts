79a2a0781e8140fc5bd90f350f0fce0a525d46ca
Commonly <51011212+commonly-ts@users.noreply.github.com>
2026-03-15 00:08:10 +1300

    Fixed barrel file type exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from "#feature/builders/utility.builders.js";
 export * from "#feature/commands/methods.commands.js";
 export * from "#feature/customId/methods.customid.js";
-export type * from "#typings";
+export * from "#typings";
