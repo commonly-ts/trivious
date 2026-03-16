1c4aabc40ce85a547bc14371c76bb124833a6e20
Commonly <51011212+commonly-ts@users.noreply.github.com>
2026-03-17 11:44:35 +1300

    fix: merged encodeCustomId and decodeCustomId into customId
    Condensed the two loose methods into one object for better readability

--- src/features/customId/methods.customid.ts
+++ src/features/customId/methods.customid.ts
@@ -1,40 +1,38 @@
 import { ComponentContext, ComponentCustomId, ComponentTag } from "#typings";
 import { TriviousError } from "#utility/errors.js";
 
-/**
- * Decode a customId into its parts
- * @param customId The custom id
- * @returns Decoded customId
- */
-export const decodeCustomId = (customId: string) => {
-	const [context, identifier, info] = customId.split(":") as [
-		ComponentContext,
-		string,
-		string | undefined,
-	];
-	const [data, ...tags] = info?.split(".") as [string | undefined, ...ComponentTag[]];
+const customId = {
+	/**
+	 * Decode a customId into its parts
+	 */
+	decode: (customId: string) => {
+		const [context, identifier, info] = customId.split(":") as [
+			ComponentContext,
+			string,
+			string | undefined,
+		];
+		const [data, ...tags] = info?.split(".") as [string | undefined, ...ComponentTag[]];
 
-	return {
-		context,
-		identifier,
-		data,
-		tags,
-	} as ComponentCustomId;
-};
+		return {
+			context,
+			identifier,
+			data,
+			tags,
+		} as ComponentCustomId;
+	},
+	/**
+	 * Encode a customId
+	 */
+	encode: (options: ComponentCustomId) => {
+		const { context, identifier, data, tags } = options;
+		let customId = `${context}:${identifier}`;
 
-/**
- * Encode a customId
- * @param options Custom id parts
- * @returns Encoded customId
- * @throws {TriviousError} If encoded length exceeds 100 characters
- */
-export const encodeCustomId = (options: ComponentCustomId) => {
-	const { context, identifier, data, tags } = options;
-	let customId = `${context}:${identifier}`;
+		if (data) customId += `:${data}`;
+		if (tags) customId += `.${tags.join(".")}`;
 
-	if (data) customId += `:${data}`;
-	if (tags) customId += `.${tags.join(".")}`;
+		if (customId.length > 100) throw new TriviousError("Encoded customId exceeds 100 characters.");
+		return customId;
+	},
+} as const;
 
-	if (customId.length > 100) throw new TriviousError("Encoded customId exceeds 100 characters.");
-	return customId;
-};
+export default customId;
