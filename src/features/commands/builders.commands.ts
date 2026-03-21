485a7c5cbe29cfb56f805515f64eb914d3986a84
Commonly <51011212+commonly-ts@users.noreply.github.com>
2026-03-21 21:54:51 +1300

    fix: added command builder helpers

---
+++ src/features/commands/builders.commands.ts
@@ -0,0 +1,55 @@
+import { ApplicationCommandType, Collection } from "discord.js";
+import {
+	ContextCommandData,
+	SlashCommandData,
+	SlashSubcommandData,
+	SlashSubcommandGroupData,
+} from "./commands.types.js";
+
+export function createSlashCommand(
+	data: Omit<SlashCommandData, "context" | "commandType" | "subcommands" | "subcommandGroups">
+): SlashCommandData {
+	return {
+		...data,
+		context: "SlashCommand",
+		commandType: ApplicationCommandType.ChatInput,
+	} satisfies SlashCommandData;
+}
+
+export function createSubcommand(
+	data: Omit<SlashSubcommandData, "context" | "commandType" | "parent">
+): SlashSubcommandData {
+	return {
+		...data,
+		context: "SlashSubcommand",
+		commandType: ApplicationCommandType.ChatInput,
+	} satisfies SlashSubcommandData;
+}
+
+export function createSubcommandGroup(
+	data: Omit<SlashSubcommandGroupData, "context" | "parent" | "subcommands">
+): SlashSubcommandGroupData {
+	return {
+		...data,
+		context: "SlashSubcommandGroup",
+		subcommands: new Collection(),
+	} satisfies SlashSubcommandGroupData;
+}
+
+export function createMessageComponentCommand(
+	data: Omit<ContextCommandData<"Message">, "commandType">
+): ContextCommandData<"Message"> {
+	return {
+		...data,
+		commandType: ApplicationCommandType.Message,
+	} satisfies ContextCommandData<"Message">;
+}
+
+export function createUserComponentCommand(
+	data: Omit<ContextCommandData<"User">, "commandType">
+): ContextCommandData<"User"> {
+	return {
+		...data,
+		commandType: ApplicationCommandType.User,
+	} satisfies ContextCommandData<"User">;
+}
