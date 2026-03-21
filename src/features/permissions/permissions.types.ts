1265070e9d6275b93c9b3cfc4f359b894d9cafaa
Commonly <51011212+commonly-ts@users.noreply.github.com>
2026-03-21 21:25:25 +1300

    fix: made userIds optional in CommandPermissionValues

--- src/features/permissions/permissions.types.ts
+++ src/features/permissions/permissions.types.ts
@@ -3,5 +3,5 @@ import { PermissionsBitField } from "discord.js";
 export interface CommandPermissionValues {
 	requiredRoleIds?: string[];
 	requiredMemberPermissions?: PermissionsBitField[];
-	userIds: string[];
+	userIds?: string[];
 }
