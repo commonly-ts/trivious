b3c8c69e39b7b2ebb1d14d933f284e9a66fedf2c
Commonly <51011212+commonly-ts@users.noreply.github.com>
2026-03-21 21:22:57 +1300

    fix: added permission check to respect subcommand permissions

--- src/features/events/presets/interactionCreate.ts
+++ src/features/events/presets/interactionCreate.ts
@@ -1,13 +1,59 @@
-import { ComponentContext, type ContextCommandData, type Event } from "#typings";
+import {
+	BaseContextCommandData,
+	ComponentContext,
+	SlashCommandData,
+	TriviousClient,
+	type ContextCommandData,
+	type Event,
+} from "#typings";
 import {
 	ApplicationCommandType,
 	ButtonInteraction,
+	ChatInputCommandInteraction,
 	GuildMember,
+	MessageContextMenuCommandInteraction,
 	ModalSubmitInteraction,
+	UserContextMenuCommandInteraction,
 } from "discord.js";
 import { handleSlashCommand, interactionReply } from "src/features/commands/methods.commands.js";
 import customId from "src/features/customId/methods.customid.js";
 import { canMemberRunCommand } from "src/features/permissions/methods.permissions.js";
+
+/**
+ * Check if the command is a subcommand and validate whether the member can run the command based on subcommand permissions
+ */
+function validateMemberPermissionsForSubcommand(
+	client: TriviousClient,
+	command: SlashCommandData | BaseContextCommandData,
+	interaction:
+		| ChatInputCommandInteraction
+		| MessageContextMenuCommandInteraction
+		| UserContextMenuCommandInteraction
+): boolean {
+	if (!("subcommands" in command)) return false;
+	if (!interaction.isChatInputCommand()) return false;
+
+	const { options } = interaction;
+
+	const subcommandName = options.getSubcommand(false);
+	const groupName = options.getSubcommandGroup(false);
+	if (!subcommandName) return false;
+
+	if (groupName) {
+		const group = command.subcommandGroups?.get(groupName);
+		if (!group) return false;
+
+		const subcommand = group.subcommands.get(subcommandName);
+		if (!subcommand) return false;
+
+		return canMemberRunCommand(client, subcommand, interaction.member as GuildMember)[0];
+	}
+
+	const subcommand = command.subcommands?.get(subcommandName);
+	if (!subcommand) return false;
+
+	return canMemberRunCommand(client, subcommand, interaction.member as GuildMember)[0];
+}
 
 export default {
 	name: "interactionCreate",
@@ -29,7 +75,9 @@ export default {
 				return;
 			}
 
-			const hasPermission = canMemberRunCommand(client, command, interaction.member as GuildMember);
+			const hasPermission =
+				validateMemberPermissionsForSubcommand(client, command, interaction) ||
+				canMemberRunCommand(client, command, interaction.member as GuildMember);
 			if (!hasPermission) {
 				await interactionReply({
 					interaction,
