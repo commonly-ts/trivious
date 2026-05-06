41eb95bd01678a848907eace910c7eaa73ebe056
Commonly <51011212+commonly-ts@users.noreply.github.com>
2026-05-06 17:42:16 +1200

    fix: updated barrel file
    organised it some and added the new builders

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
+// Types
+export * from "#typings";
+
+// Methods
 export * from "#feature/builders/utility.builders.js";
-export * from "#feature/commands/builders.commands.js";
 export * from "#feature/commands/methods.commands.js";
 export { default as customId } from "#feature/customId/methods.customid.js";
-export * from "#typings";
+
+// Builders
+export * from "#feature/commands/builders.commands.js";
+export * from "#feature/components/builders.components.js";
+export * from "#feature/events/builders.events.js";
+export * from "#feature/modules/builders.modules.js";
